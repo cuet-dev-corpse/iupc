bc5be3675ae7cb02d2807a3e0a921aeb24552c38
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-01-11 10:01:16 +0600

    text white color changed

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 
 export default function Schedule() {
-    const [items, setItems] = useState(items1);
+    const [items, setItems] = useState([0, items1[0]]);
     return (
         <section className='container2 mt-[100px]'>
             <div className='title'>Schedule Plan</div>
@@ -23,46 +23,42 @@ export default function Schedule() {
                 }
             </div>
 
-            <div className="py-6 flex flex-col justify-center sm:py-12">
+            <div className="py-6 flex flex-col justify-center sm:py-12 mt-8">
                 <div className='flex items-center justify-center gap-4'>
-
-                    <div className='bg-[#fff3] text-white py-4 rounded-md cursor-pointer shadow-xl'>
-                        <div className='bg-blue-500 text-white inline-block px-4 py-1 rounded-r-3xl'>Day 01</div>
-                        <div className='flex items-center gap-3 px-4'>
-                            <span className='text-5xl font-bold'>24</span>
-                            <span className='flex flex-col justify-center items-center'>
-                                <span className='text-xl font-medium'>NOV</span>
-                                <span className=''>2024</span>
-                            </span>
-                        </div>
-                    </div>
-                    <div className='bg-[#fff3] text-white py-4 rounded-md border-2 border-green-500 cursor-pointer shadow-xl'>
-                        <div className='bg-green-500 text-white inline-block px-4 py-1 rounded-r-3xl'>Day 01</div>
-                        <div className='flex items-center gap-3 px-4'>
-                            <span className='text-5xl font-bold'>23</span>
-                            <span className='flex flex-col justify-center items-center'>
-                                <span className='text-xl font-medium'>NOV</span>
-                                <span className=''>2024</span>
-                            </span>
-                        </div>
-                    </div>
+                    {
+                        [[23, 'blue-500'], [24, 'green-500']].map((e, i) => (
+                            <div
+                                key={i}
+                                onClick={() => setItems(items[0] != i ? [i, items1[i]] : items)}
+                                className={`bg-[#fff3] text-white py-4 rounded-md border-2 ${i % 2 == items[0] ? `border-${e[1]}` : 'border-0'} cursor-pointer shadow-xl`}>
+                                <div className={`bg-${e[1]} text-xs text-white inline-block md:px-4 px-2 py-1 rounded-r-3xl`}>Day 0{i+1}</div>
+                                <div className='flex items-center gap-3 px-4'>
+                                    <span className='md:text-5xl text-2xl font-bold'>{e[0]}</span>
+                                    <span className='flex flex-col justify-center items-center'>
+                                        <span className='md:text-xl text-xs font-medium'>NOV</span>
+                                        <span className='text-xs md:text-sm'>2024</span>
+                                    </span>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
-                <div class="max-w-[800px] w-full mx-auto px-2 mt-8">
+                <div class="max-w-[800px] w-full mx-auto px-2 md:mt-8 mt-0">
 
                     <div class="relative text-sm">
-                        <div class="block w-1 bg-blue-300 absolute h-[90%] top-5 sm:left-1/2 left-10 transform -translate-x-1/2"></div>
+                        <div class="block w-1 bg-blue-300 absolute h-[80%] top-5 sm:left-1/2 left-10 transform -translate-x-1/2"></div>
                         {
-                            items.map((item, i) => (
-                                <div key={i} class="mt-6  mb-12">
-                                    <div class='flex flex-col sm:flex-row items-center'>
+                            items[1].map((item, i) => (
+                                <div key={i} class="mt-6 mb-16">
+                                    <div class='flex flex-col sm:flex-row items-center text-xs md:text-sm'>
                                         <div class={`flex ${i % 2 ? 'sm:justify-end' : 'sm:justify-start'} w-full mx-auto items-center`}>
-                                            <div class={`sm:w-1/2 ${i % 2 ? 'sm:pl-16 pl-32' : 'sm:pr-16 sm:pl-0 pl-32 !sm-pl-0 sm:text-right'}`}>
+                                            <div class={`sm:w-1/2 ${i % 2 ? 'sm:pl-16 pl-24' : 'sm:pr-16 sm:pl-0 pl-24 !sm-pl-0 sm:text-right'}`}>
                                                 <div class="p-4 bg-white rounded shadow text-gray-700">
-                                                    <div className="text-xl font-d">{item.title}</div>
+                                                    <div className="font-medium">{item.title}</div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class={`rounded-full bg-blue-500 border-white border-4 absolute sm:left-1/2 left-0 translate-y-0 transform sm:-translate-x-1/2 flex items-center justify-center flex-col aspect-square p-2`}>
+                                        <div class={`rounded-full bg-blue-500 border-white border-4 absolute sm:left-1/2 left-0 sm:translate-y-0 -translate-y-4 transform sm:-translate-x-1/2 flex items-center justify-center flex-col aspect-square p-2`}>
                                             <span>{item.start}</span>
                                             <span>{item.end}</span>
                                         </div>
@@ -80,34 +76,35 @@ export default function Schedule() {
 }
 
 const items1 = [
-    {
-        start: '10: 30 AM',
-        end: '10: 00 AM',
-        title: 'Inauguration',
-    },
-    {
-        start: '9: 00 AM',
-        end: '10: 00 AM',
-        title: 'Dedicated Event/Seminar slot for Title and Golden Sopnsor',
-    },
-    {
-        start: '9: 00 AM',
-        end: '10: 00 AM',
-        title: 'Mock Context',
-    },
-]
-
-const items2 = [
-    {
-        start: '9: 00 AM',
-        end: '2: 00 PM',
-        title: 'Programming Contest (National)',
-    },
-    {
-        start: '3: 00 PM',
-        end: '5: 00 PM',
-        title: 'Closing Ceremony & Prize Giving',
-    },
+    [
+        {
+            start: '10: 30 AM',
+            end: '10: 00 AM',
+            title: 'Inauguration',
+        },
+        {
+            start: '9: 00 AM',
+            end: '10: 00 AM',
+            title: 'Dedicated Event/Seminar slot for Title and Golden Sopnsor',
+        },
+        {
+            start: '9: 00 AM',
+            end: '10: 00 AM',
+            title: 'Mock Context',
+        },
+    ],
+    [
+        {
+            start: '9: 00 AM',
+            end: '2: 00 PM',
+            title: 'Programming Contest (National)',
+        },
+        {
+            start: '3: 00 PM',
+            end: '5: 00 PM',
+            title: 'Closing Ceremony & Prize Giving',
+        },
+    ]
 ]
 
 const times = [
